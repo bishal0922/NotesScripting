ed24df87feddea7db32e15bec024f7d7182da4f4
Bishal Giri <bishal0922@gmail.com>
2022-11-24 16:42:05 -0600

    create a map instead of reading file + cont edit

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ os.chdir('../../../bishal/notes')
 # print(os.getcwd()) -> will now go to the notes directory
 print(os.getcwd())
 
-#read class_list.txt and store it in a list
-if os.path.exists('class_list.txt'):
-    with open('class_list.txt') as f:
-        class_list = f.readlines()
-        # strip the newline character '\n' from each line
-        class_list = [x.strip() for x in class_list]
+# #read class_list.txt and store it in a list
+# if os.path.exists('class_list.txt'):
+#     with open('class_list.txt') as f:
+#         class_list = f.readlines()
+#         # strip the newline character '\n' from each line
+#         class_list = [x.strip() for x in class_list]
+
+# we should rather create file as a map
+class_list = {
+    'CSE3315': 'Theory of Computation',
+    'COMS2302': 'Business Communication',
+    'CSE3310': 'Intro to Software Engineering',
+}
 
 
 folder_name = datetoday.get_date_file_name()
@@ -29,11 +36,11 @@ if not os.path.isdir(dir):
 
     #read class_list.txt and append it to month_day
     for my_class in class_list:
-        new_file_name = my_class+(month_day)+'.md'
+        new_file_name = my_class + '_' + (month_day) + '.md'
         fp = open(new_file_name, 'w')
         
         #write the header
-        fp.write('# '+my_class+'\n'+
+        fp.write('# '+ my_class + ' - ' +class_list.get(my_class) +'\n'+
             '### '+ datetoday.get_full_date() + '\n')    
 
 else:
