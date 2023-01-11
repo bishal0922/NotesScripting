14cc789a31553e7b5b4e7bcf4c747c63e53c5d10
Bishal Giri <bishal0922@gmail.com>
2023-01-11 00:33:00 -0600

    refactor and remove firebase stuff

--- upload.py
+++ upload.py
@@ -1,40 +1,31 @@
+#!usr/bin/python3
+
+"""
+This script is responsible for turning the created .md files from main.py to html/ pdf files
+"""
+
 import os
-import datetoday
-# import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import firestore
+import date_formatter
 
-# command is : python3 -m markdown COMS2302_Nov24.md > COMS2302_Nov24.html
+# To turn .md to .html ==
+# python3 -m markdown COMS2302_Nov24.md > COMS2302_Nov24.html
+# To run .md to pdf ===
+# os.system('mdpdf '+ file)
 
-# assign directory
+# assign directory back to our notes folder where the .md files were created
 os.chdir('../../../bishal/notes')
 
-dir = datetoday.get_date_file_name()
-directory = os.chdir(dir)
+#ThuNov25
+directory = os.chdir(date_formatter.get_date_file_name())
+
+#current working directory
 print(os.getcwd())
-# iterate over files in
-# that directory
+
+# iterate over files in that direcotry
 for filename in os.listdir(directory):
     if os.path.isfile(filename):
-        file = filename
         #turning the markdown file into html
-        os.system('mdpdf '+ file)
-        #os.system('python3 -m markdown ' + file + ' > ' + file[:-2] + 'html')
-
-
-# Use a service account.
-#print working directory
-# Users/bishal/Documents/bishal/notes/ThuNov24
+        os.system('python3 -m markdown ' + filename + ' > ' + filename[:-2] + 'html')
 
 #changing directory to our project folder
-os.chdir('../../../projects/NotesScripting/NotesScripting')
-
-
-# cred = credentials.Certificate('mynotes-cede7-firebase-adminsdk-nxksw-f19f4e0b57.json')
-
-# app = firebase_admin.initialize_app(cred)
-
-# db = firestore.client()
-# now we have all the files in html format and we can upload them to the amazon s3 server
-
-# add a file to the database
+#os.chdir('../../../projects/NotesScripting/NotesScripting')
